06d35e9f4fd197d8dd2b1901a55c6391f4cb5bb5
Siebe <siebe.vanderven1@edu-kw1c.nl>
2021-04-07 16:53:03 +0200

    Added reponsive navigation, fixed bugs

---
+++ javascript/nav.js
@@ -0,0 +1,11 @@
+function toggleNav()
+  {
+      const $ul = document.querySelector('ul');
+      $ul.classList.toggle('has-open-nav'); 
+  }
+
+  function Navcolor()
+  {
+    const $nav = document.querySelector('nav');
+    $nav.classList.toggle('white-background');
+  }
